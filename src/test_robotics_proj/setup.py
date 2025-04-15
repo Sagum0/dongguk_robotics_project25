cda403dbb4baf6326f54339b7449581abdf49d5f
Sagum0 <jyw010704@naver.com>
2025-04-15 17:56:16 +0900

    모터 연결 성공

---
+++ src/test_robotics_proj/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'test_robotics_proj'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='pc',
+    maintainer_email='jyw010704@naver.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'motor_ready = test_robotics_proj.motor_ready:main',
+            #'control_ready = robotics_proj.control_node:main',
+            #'test_control = robotics_proj.test_control:main',
+        ],
+    },
+)
